32267979766613b6d525380092671a7bdbf5b8bc
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-02-21 19:24:57 +0100

    Add about module and navigation in header

---
+++ client/modules/About/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2>About</h2>
+        <p>Lorem ipsum</p>
+      </div>
+    );
+  }
+}
+
+export default About;
+
--- client/modules/App/components/Header/Header.js
+++ client/modules/App/components/Header/Header.js
@@ -19,6 +19,13 @@ export function Header(props, context) {
           {languageNodes}
         </ul>
       </div>
+      <div className={styles['navigation']}>
+        <ul>
+          <li><a href='/home'>Home</a></li>
+          <li><a href='/about'>About</a></li>
+          <li><a href='/posts'>Posts</a></li>
+        </ul>
+      </div>
       <div className={styles.content}>
         <h1 className={styles['site-title']}>
           <Link to="/" ><FormattedMessage id="siteTitle" /></Link>
