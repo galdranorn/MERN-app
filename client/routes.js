291d59f1760850a0333a826e3a160a5a0232371a
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-02-24 14:12:28 +0100

    Remove vote requirement from PostDetailPage and add posts route

--- client/routes.js
+++ client/routes.js
@@ -57,5 +57,13 @@ export default (
         });
       }}
     />
+    <Route
+      path="/posts"
+      getComponent={(nextState, cb) => {
+        require.ensure([], require => {
+          cb(null, require('./modules/Post/pages/PostListPage/PostListPage').default);
+        });
+      }}
+    />
   </Route>
 );
